f410a84eb43e1e43607649085998135f5646a739
grantgariepy <grantgariepy@protonmail.com>
2022-08-11 09:48:11 -0600

    slug page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { AiFillGithub } from 'react-icons/ai';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer-container">
+      <p>2022 Grant Gariepy All rights reserverd</p>
+      <p className="icons">
+        <AiFillGithub />
+      </p>
+    </div>
   )
 }
 
