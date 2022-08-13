243d7ebfc8535f0d3f5c7dda56435e13ebfe9b0a
grantgariepy <grantgariepy@protonmail.com>
2022-08-13 09:23:49 -0600

    rel

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { AiFillGithub } from 'react-icons/ai';
+import { Link } from 'react';
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <p>2022 Grant Gariepy All rights reserverd</p>
       <p className="icons">
-        <a href="https://github.com/grantgariepy/dont-blend-in" target="_blank">
+        <a href="https://github.com/grantgariepy/dont-blend-in" target="_blank" rel="noreferrer">
 
           <AiFillGithub />
         </a>
